14fca67d96e9a3075b83291a33c3b8f808facd9b
Samu Garron <garron.garron@gmail.com>
2021-04-23 08:45:37 -0300

    Version 2

--- src/Samu/Model.php
+++ src/Samu/Model.php
@@ -14,4 +14,18 @@ class Model
         $class = get_called_class();
         return new $class(self::$DB->run());
     }
+
+    public function toJson($result = null)
+    {
+        $line = [];
+        if ($result === true) {
+            return json_encode($line);
+        }
+        if ($result) {
+            while ($obj = $result->fetch_object()) {
+                array_push($line, $obj);
+            }
+        }
+        return json_encode($line);
+    }
 }
